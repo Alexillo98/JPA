9e8f01b80794e2e017bcbcbf649c15ab28ffb49c
Alexillo98 <musateduardalexandru@gmail.com>
2024-04-11 10:13:25 +0200

    JPA

---
+++ src/main/java/animals/Animal.java
@@ -0,0 +1,13 @@
+package animals;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Animal {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+}
